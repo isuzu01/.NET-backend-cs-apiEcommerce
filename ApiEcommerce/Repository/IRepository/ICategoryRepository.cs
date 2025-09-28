9c3c6da9da24e8b30d7bb8d3e60fad9d89a221dd
Jhuli Trujillo <jhulitrujillomeza@gmail.com>
2025-09-27 20:12:38 -0500

    add interface ICategoryRepository

---
+++ ApiEcommerce/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ApiEcommerce.Repository;
+
+public interface ICategoryRepository
+{
+  //metodos para gestionar categorias desde esta capa de abstraccion
+  ICollection<Category> GetCategories();
+  Category GetCategory(int id);
+  bool CategoryExists(int id);
+  bool CategoryExists(string name);
+  bool CreateCategory(Category category);
+  bool UpdateCategory(Category category);
+  bool DeleteCategory(Category category);
+
+  bool Save();
+
+}
